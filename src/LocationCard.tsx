d25bb72822b91fcd82f9dfb1cca68173f8e5dc20
Avery <19372006+starsflower@users.noreply.github.com>
2021-11-06 22:24:18 +0000

    feat: Remove location button and country info

--- src/LocationCard.tsx
+++ src/LocationCard.tsx
@@ -1,9 +1,8 @@
-import { Card, CardActionArea, CardActions, CardContent, Icon, IconButton, Typography } from "@mui/material";
+import { Card, CardActionArea, CardActions, CardContent, IconButton, Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
-import getWeatherForecast, { IWeatherForecastReturnData, useWeatherData } from "./getWeather";
-import { useToggleFavouriteLocation } from "./locationsContext";
+import { useWeatherData } from "./getWeather";
+import { useRemoveLocation, useToggleFavouriteLocation } from "./locationsContext";
 import { useTemperaturePreference } from "./temperatureContext";
 
 interface ILocationCardProps {
@@ -16,21 +15,26 @@ export default function LocationCard({ location, favourite }: ILocationCardProps
     const temperature = useTemperaturePreference();
     const toggleFavourite = useToggleFavouriteLocation();
     const navigate = useNavigate();
-
+    const deleteLocation = useRemoveLocation();
 
     return (
         <>
             <Card>
                 <CardActionArea onClick={() => {navigate("/location/" + encodeURIComponent(location))}}>
                     <CardContent sx={{ display: 'flex' }}>
-                        <Typography fontSize="2rem" sx={{flex: '1 0 auto'}}>
-                            {data?.location?.name}
-                        </Typography>
-                        <Box sx={{display: 'flex', flexDirection: 'column', textAlign: "right"}}>
+                        <Box sx={{flex: '1 0 auto'}}>
+                            <Typography fontSize="2rem">
+                                {data?.location?.name}
+                            </Typography>
+                            <Typography color="text.secondary">
+                                {data?.location?.country}
+                            </Typography>
+                        </Box>
+                        <Box sx={{textAlign: "right"}}>
                             <Typography fontSize="2rem">
                                 {temperature(data?.current?.temp_c, data?.current?.temp_f)}
                             </Typography>
-                            <Typography fontSize="smaller">
+                            <Typography color="text.secondary">
                                 Feels like {temperature(data?.current?.feelslike_c, data?.current?.feelslike_f)}
                             </Typography>
                         </Box>
@@ -42,6 +46,13 @@ export default function LocationCard({ location, favourite }: ILocationCardProps
                         sx={{color: favourite ? "gold" : "black", cursor: "pointer"}}
                         onClick={(e) => {
                             toggleFavourite(location);
+                        }}
+                    />
+                    <IconButton
+                        className="fas fa-trash"
+                        sx={{color: "red", cursor: "pointer"}}
+                        onClick={(e) => {
+                            deleteLocation(location);
                         }}
                     />
                 </CardActions>
